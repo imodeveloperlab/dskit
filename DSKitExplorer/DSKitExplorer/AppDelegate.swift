e83b5c479a96b23096fce658dd42d821ff995369
Borinschi Ivan <imodeveloperlab@gmail.com>
2022-03-31 19:03:52 +0300

    Fix: crash when changing from light to dark moode

--- DSKitExplorer/DSKitExplorer/AppDelegate.swift
+++ DSKitExplorer/DSKitExplorer/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         DSAppearance.shared.main = DSKitAppearance()
+        DSAppearance.shared.userInterfaceStyle = .unspecified
         return true
     }
 
